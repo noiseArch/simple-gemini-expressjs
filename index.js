eff44ee1fa62b9c7f6cf95d6c05803d05a1e92d6
Matías <mativalto@gmail.com>
2024-03-03 20:45:20 -0300

    first commit

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 
 const ai = async (prompt) => {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    return text
+    try {
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        return text
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 app.use(express.json()) // for parsing application/json
